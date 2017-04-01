830f5a07fa9119bffc9b7c5521f54a96273bd1fe
mohammad_lom <mohammad2682593@gmail.com>
2017-04-01 08:56:48 +0430

    Source Directory

---
+++ src/course/ai/ui/City.java
@@ -0,0 +1,28 @@
+package course.ai.ui;
+
+/**
+ * Created by mohammad_lom on 3/31/17.
+ */
+public enum City {
+    Oradea,
+    Zerind,
+    Arad,
+    Sibiu,
+    Timisoara,
+    Fagaras,
+    RimnicuVilvea,
+    Lugoj,
+    Mehadia,
+    Pitesti,
+    Drobeta,
+    Craiova,
+    Bucharest,
+    Giurgiu,
+    Urziceni,
+    Hirsova,
+    Eforie,
+    Vaslui,
+    Iasi,
+    Neamt
+
+}
